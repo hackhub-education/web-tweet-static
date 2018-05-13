10880a1db310621ae7ddf240a99d395c1d53c382
Yan Hong <onlyhongyan@gmail.com>
2018-05-13 02:30:33 -0700

    signup post

--- js/script.js
+++ js/script.js
@@ -7,15 +7,13 @@ var loadAllTweets = () => {
         url: baseUrl + 'tweet',
         success: (data) => {
             for (let tweet of data.tweets) {
-                console.log(tweet);
-    
                 let tweetContainer = $('<div>').addClass('tweet').append()
                 let row = $('<div>').addClass('row').appendTo(tweetContainer)
                 $('<img>').addClass('avatar-sm').attr({
                     'src': tweet.author.avatarUrl,
                     'alt': 'avatar'
                 }).appendTo(row)
-                $('<h4><b>' + tweet.author.name +'</b></h4>').appendTo(row)
+                $('<h4><b>' + tweet.author.name + '</b></h4>').appendTo(row)
                 $('<h5>@' + tweet.author.username + '</h5>').appendTo(row)
                 $('<h5>').text(moment(tweet.createdAt).calendar()).appendTo(row)
                 let content = $('<p>').text(tweet.content).appendTo(tweetContainer)
@@ -32,4 +30,38 @@ var loadAllTweets = () => {
 
 }
 
+var signupForm = () => {
+
+    $('#signup-btn').click(() => {
+
+        let newUser = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        }
+
+        console.log(newUser)
+
+        $.ajax({
+            type: "POST",
+            url: baseUrl + "auth/signup",
+            data: newUser,
+            success: function (data) {
+                if (data.error) {
+                    console.log(data.error.message)
+                } else {
+                    console.log(data)
+                    // localStorage.token = data.token;
+                    // alert('Got a token from the server! Token: ' + data.token);
+                }
+            },
+            error: function () {
+                alert("Signup Failed");
+            }
+        });
+
+    })
+
+}
+
 loadAllTweets()
+signupForm()
