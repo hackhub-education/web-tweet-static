520df14af456f4c80055784e23097f914ce1e7fb
Yan Hong <onlyhongyan@gmail.com>
2018-05-13 02:07:52 -0700

    use momentjs to format date

--- js/script.js
+++ js/script.js
@@ -15,12 +15,15 @@ $.ajax({
             }).appendTo(row)
             $('<h4><b>' + tweet.author.name +'</b></h4>').appendTo(row)
             $('<h5>@' + tweet.author.username + '</h5>').appendTo(row)
-            $('<h5>').text(tweet.createdAt).appendTo(row)
+            $('<h5>').text(moment(tweet.createdAt).calendar()).appendTo(row)
             let content = $('<p>').text(tweet.content).appendTo(tweetContainer)
             if (tweet.imageUrl) {
                 $('<br><img src="' + tweet.imageUrl + '" alt="tweet">').appendTo(content)
             }
             $('#tweet-list').append(tweetContainer)
         }
+    },
+    error: (err) => {
+        console.log(err.statusText)
     }
 });
